3fe6bb4457f00e4c5d7b49488ec2184019732ae9
Todd Burgess <todd@toddburgessmedia.com>
2019-01-13 17:30:26 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# random_cat_app
+
+Random Cat Photos
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Random Cat',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Random Cat Photo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String _file = "https:\/\/purr.objects-us-east-1.dream.io\/i\/rd0Nc.jpg";
+
+  void _getCatPhoto() async {
+    String url = "https://aws.random.cat/meow";
+    http.Response response = await http.get(url);
+    setState(() {
+      final fileJSON = json.decode(response.body);
+      _file = fileJSON["file"];
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Image.network(_file)
+            ,
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _getCatPhoto,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
